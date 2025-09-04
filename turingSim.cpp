00725b04460df3cdcc3900fb7bf5a0422354c2cf
WillAlley-official <walley@binghamton.edu>
2025-09-03 20:32:12 -0400

    Filtered file input into proper data types

--- turingSim.cpp
+++ turingSim.cpp
@@ -10,7 +10,24 @@ using namespace std;
 // GLOBAL VARIABLE DECLARATIONS
 //@-------------------------------------------@
 
+// State Structure
+struct State {
+	char currTapeValue = '\0';
+	int destinationState = -1;
+	char newTapeValue = '\0';
+	char tapeMovement = '\0';
+	State* next = nullptr;
+};
+
+// Variables
 string currentLine = "null";
+int tapeHead = 0;
+State* currState = nullptr;
+int counter = 0;
+
+// State Arrays
+string stateType[1001];
+State* transitionData[1001];
 
 //@-------------------------------------------@
 // HELPER FUNCTIONS
@@ -35,7 +52,29 @@ int main(int argc, char* argv[]) {
 
 		while(iss >> token) { tokens.push_back(token); }
 
-		cout << tokens[0] << endl;		
+		// For State Definitions:
+		if(tokens[0] == "state") {
+
+			int index = stoi(tokens[1]);
+			stateType[index] = tokens[2];
+
+		// For Transition Definitions:
+		} else {
+
+			State* newState = new State();
+			char c = tokens[2][0];
+			newState->currTapeValue = c;
+			int num = stoi(tokens[3]);
+			newState->destinationState = num;
+			c = tokens[4][0];	
+			newState->newTapeValue = c;
+			c = tokens[5][0];	
+			newState->tapeMovement = c;
+
+			// TODO: Finish setting up the transitionData array!	
+	
+			delete newState;
+		}
 	}	
 
 	// PHASE 2 : Running Input Tape & Determining Result
